4fb00d0eeeba12b419c12cc665cbe22fc8cc7228
hjkim <hjhjgudwls1022@gmail.com>
2024-09-27 01:42:07 +0900

    의존관계 자동 주입

---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "memberA", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 20000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
